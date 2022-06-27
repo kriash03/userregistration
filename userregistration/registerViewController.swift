3c11dd09bfcdb8b05bba7ab59177ee0ef23f93c3
kriash03 <97736931+kriash03@users.noreply.github.com>
2022-06-28 02:02:06 +0530

    Initial Commit

---
+++ userregistration/registerViewController.swift
@@ -0,0 +1,24 @@
+//
+//  registerViewController.swift
+//  userregistration
+//
+//  Created by krishna on 28/06/22.
+//
+
+import UIKit
+
+class registerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+   
+
+}
---
+++ userregistration/signupViewController.swift
@@ -0,0 +1,27 @@
+//
+//  signupViewController.swift
+//  userregistration
+//
+//  Created by krishna on 28/06/22.
+//
+
+import UIKit
+
+class signupViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    @IBAction func signinButtonTapped(_ sender: Any) {
+        print("Sign in button tapped")
+    }
+    @IBAction func registerNewAccountButtonTapped(_ sender: Any) {
+        print("Register account button tapped")
+    }
+}
